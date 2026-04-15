0ae0e2c820497d34fa00f348f088e6126478a1dd
Jen Yang Koh <jenyang.koh@gmail.com>
2026-04-15 12:19:39 -0600

    chore: remove old Next.js/Supabase codebase

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-import MillionLint from '@million/lint';
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [],
-    },
-};
-
-export default nextConfig;
